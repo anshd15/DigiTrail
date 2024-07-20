422ac25c46198ed7cefdfbf9ae6cf94b634c8d5b
Shobhit Tiwari <122107079+Shobhit141141@users.noreply.github.com>
2024-07-21 01:19:26 +0530

    chore: Add daisyui plugin to tailwind.config.js and update npm dependencies

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,30 @@
 import { useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from './pages/Home';
-
 import "./App.css";
-
+import TrackingPage from "./pages/Tracking";
+import Home from "./pages/Home";
+import {Toaster} from "react-hot-toast"
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home setProgress={setProgress} />} />
-        </Routes>
+        <div className="main">
+          <Toaster />
+          <Routes>
+            <Route
+              path="/"
+              element={<Home/>}
+            />
+            <Route
+              path="/track"
+              element={<TrackingPage/>}
+            />
+          </Routes>
+        </div>
       </BrowserRouter>
-    </div>
+    </>
   );
 }
 
